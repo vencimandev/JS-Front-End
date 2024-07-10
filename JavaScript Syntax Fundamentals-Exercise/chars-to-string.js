c30be1cea247e85bbd3383cbe17c71ccaff536cb
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-07-10 22:33:52 +0300

    Add files via upload

---
+++ JavaScript Syntax Fundamentals-Exercise/chars-to-string.js
@@ -0,0 +1,6 @@
+function charsToString(char1, char2, char3) {
+    let combinedString = char1 + char2 + char3;
+    console.log(combinedString);
+}
+
+
---
+++ JavaScript Syntax Fundamentals-Exercise/reversed-chars.js
@@ -0,0 +1,5 @@
+function reversedChars(char1, char2, char3) {
+    let reversedString = `${char3} ${char2} ${char1}`;
+    console.log(reversedString);
+}
+
