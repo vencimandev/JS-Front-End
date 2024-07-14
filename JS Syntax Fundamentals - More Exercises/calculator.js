1c243c2ccb92d858106a6d9271ea78175f19171e
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-07-14 13:50:39 +0300

    Add files via upload

---
+++ JS Syntax Fundamentals - More Exercises/calculator.js
@@ -0,0 +1,25 @@
+function calculator(num1, operator, num2) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            console.log('Invalid operator');
+            return;
+    }
+
+    console.log(result.toFixed(2));
+}
+
+
