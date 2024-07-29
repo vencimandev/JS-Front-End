2b770260e697523b63a48f8f66b763681ab4cf43
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-07-29 22:11:20 +0300

    Update 11-book-shelf.js

--- Objects and Classes - More Exercises/11-book-shelf.js
+++ Objects and Classes - More Exercises/11-book-shelf.js
@@ -19,10 +19,8 @@ function manageBooks(input) {
         }
     });
 
-    // Convert shelves object to an array for sorting
     let shelvesArray = Object.entries(shelves).sort((a, b) => b[1].books.length - a[1].books.length);
 
-    // Print the results
     shelvesArray.forEach(([id, shelf]) => {
         console.log(`${id} ${shelf.genre}: ${shelf.books.length}`);
         shelf.books.sort((a, b) => a.title.localeCompare(b.title)).forEach(book => {
